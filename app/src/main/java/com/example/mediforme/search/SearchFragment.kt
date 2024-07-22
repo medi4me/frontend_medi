7ef6233b729477f6c97dce3a9699eec0a4210a81
Your Name <you@example.com>
2024-07-22 14:36:30 +0900

    feat:바텀 내비게이션바 수정(이름+아이콘 변경)

--- app/src/main/java/com/example/mediforme/search/SearchFragment.kt
+++ app/src/main/java/com/example/mediforme/search/SearchFragment.kt
@@ -1,5 +1,4 @@
-package com.example.mediforme.notification
-
+package com.example.mediforme.search
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,13 +6,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mediforme.R
 
-class NotificationsFragment : Fragment() {
+class SearchFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 }
