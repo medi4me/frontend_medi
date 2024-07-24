b596cc7bf3f818e29b913ed986dfb2ab57ec8938
Chamin Im <ckals413@naver.com>
2024-07-24 21:14:09 +0900

    FEAT:마이프래그먼트 리사이클러뷰 추가 중

---
+++ app/src/main/java/com/example/mediforme/mypage/ContentDrug.kt
@@ -0,0 +1,9 @@
+package com.example.mediforme.mypage
+
+data class ContentDrug(
+    val contentDrugImg: Int,
+    val contentDrugName: String,
+    val contentDrugTime: String,
+    val contentDrugFrequency: String,
+    val contentDrugBell :Int
+)
