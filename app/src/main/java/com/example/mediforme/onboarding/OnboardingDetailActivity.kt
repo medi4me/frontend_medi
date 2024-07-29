40a142261d472eb31b6acd6defdfb861e7f95c59
Your Name <you@example.com>
2024-07-29 17:44:06 +0900

    FEAT: Onboarding TimePicker 구현

--- app/src/main/java/com/example/mediforme/onboarding/OnboardingDetailActivity.kt
+++ app/src/main/java/com/example/mediforme/onboarding/OnboardingDetailActivity.kt
@@ -1,13 +1,18 @@
 package com.example.mediforme.onboarding
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mediforme.R
 import com.example.mediforme.databinding.ActivityOnboardingDetailBinding
 
 class OnboardingDetailActivity : AppCompatActivity() {
@@ -18,15 +23,18 @@ class OnboardingDetailActivity : AppCompatActivity() {
         binding = ActivityOnboardingDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 복용 시간 버튼 클릭 리스너 설정
+        binding.doseTimeSpinner.setOnClickListener {
+            showTimePickerDialog()
+        }
+
         // 식사 시간 스피너에 아이템 선택 리스너를 설정
         binding.mealTimeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            // 아이템이 선택되었을 때 호출되는 메소드
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
                 val selectedItem = parent.getItemAtPosition(position).toString()
                 Toast.makeText(this@OnboardingDetailActivity, "Selected: $selectedItem", Toast.LENGTH_SHORT).show()
             }
 
-            // 아무 아이템도 선택되지 않았을 때 호출되는 메소드
             override fun onNothingSelected(parent: AdapterView<*>) {
                 // 선택되지 않았을 때의 동작 (여기서는 아무것도 하지 않음)
             }
@@ -48,5 +56,24 @@ class OnboardingDetailActivity : AppCompatActivity() {
         binding.veriBtn.setOnClickListener {
             startActivity(Intent(this, OnboardingMedicineActivity::class.java))
         }
+    }
+
+    private fun showTimePickerDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_time_picker, null)
+        val timePicker = dialogView.findViewById<TimePicker>(R.id.timePicker)
+        timePicker.setIs24HourView(true)
+
+        val alertDialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .create()
+
+        dialogView.findViewById<Button>(R.id.btnOk).setOnClickListener {
+            val hour = timePicker.hour
+            val minute = timePicker.minute
+            binding.doseTimeSpinner.text = String.format("%02d:%02d", hour, minute)
+            alertDialog.dismiss()
+        }
+
+        alertDialog.show()
     }
 }
