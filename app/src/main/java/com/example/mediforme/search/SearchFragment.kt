c39ab705f7a894e4bb33940c34c8aa2c989ec8c3
Chamin Im <ckals413@naver.com>
2024-07-30 14:54:13 +0900

    FEAT: search프래그먼트 배경 음영처리

--- app/src/main/java/com/example/mediforme/search/SearchFragment.kt
+++ app/src/main/java/com/example/mediforme/search/SearchFragment.kt
@@ -8,16 +8,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mediforme.R
 import com.example.mediforme.databinding.FragmentSearchBinding
+import com.example.mediforme.databinding.FragmentSearchWithBackgroundBinding
 
 class SearchFragment : Fragment() {
-    lateinit var binding: FragmentSearchBinding
+    lateinit var binding: FragmentSearchWithBackgroundBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        binding = FragmentSearchWithBackgroundBinding.inflate(inflater, container, false)
 
         binding.searchWithCamera.setOnClickListener {
             // Use requireContext() to get the context
