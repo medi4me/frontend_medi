8bfc6280dc81fb54cbdb6a14acef223186f0b560
Chamin Im <ckals413@naver.com>
2024-08-02 23:59:28 +0900

    FEAT: 채팅 레이아웃 추가

---
+++ app/src/main/java/com/example/mediforme/home/chat/ChatFragment.kt
@@ -0,0 +1,22 @@
+package com.example.mediforme.home.chat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mediforme.databinding.FragmentChatBinding
+
+class ChatFragment :Fragment(){
+    lateinit var binding: FragmentChatBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChatBinding.inflate(inflater,container,false)
+
+        return binding.root
+    }
+}
