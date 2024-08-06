3ec58309f44374c8bdede78e235c146b7ff17477
Your Name <you@example.com>
2024-08-06 17:01:50 +0900

    FEAT:Splash 화면 개선(버튼 기능 추가)

--- app/src/main/java/com/example/mediforme/JoinNameActivity.kt
+++ app/src/main/java/com/example/mediforme/JoinNameActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mediforme.onboarding.OnboardingAgeActivity
+import com.example.mediforme.onboarding.OnboardingMedicineActivity
 
 class JoinNameActivity : AppCompatActivity() {
 
@@ -15,16 +16,11 @@ class JoinNameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_join_name)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val nextBtn: Button = findViewById(R.id.next_btn)
 
         nextBtn.setOnClickListener {
-            val intent = Intent(this, OnboardingAgeActivity::class.java)
+            val intent = Intent(this, OnboardingMedicineActivity::class.java)
             startActivity(intent)
         }
 
