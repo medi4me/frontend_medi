5a723920ba031e67cfff7e66a05eab3ea0986682
sojung0628 <ksj020628@naver.com>
2024-08-06 15:48:47 +0900

    CHORE : 탭레이아웃구현

--- app/src/main/java/com/example/mediforme/login/SearchIDFragment.kt
+++ app/src/main/java/com/example/mediforme/login/SearchIDFragment.kt
@@ -1,21 +1,23 @@
 package com.example.mediforme.login
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.mediforme.R
 
-class SearchIDFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.fragment_searchid)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class SearchIDFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_searchid, container, false)
     }
 }
