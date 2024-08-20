88025c3f44d2fa34298c0ddaa2ee82a31c880a22
Chamin Im <ckals413@naver.com>
2024-08-20 15:30:09 +0900

    FEAT: 상태조회, 캘린더 조회 요청, 응답 데이터클래스, 인터페이스 추가

---
+++ app/src/main/java/com/example/mediforme/Data/RetrofitInterface.kt
@@ -0,0 +1,67 @@
+package com.example.mediforme.Data
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.http.*
+
+//**************************************************************************//
+
+interface Status {
+    @POST("/status")
+    fun addStatus(
+        @Body statusRequest: StatusRequest
+    ): Call<StatusResponse>
+}
+
+//상태조회 요청
+data class StatusRequest(
+    @SerializedName("status") val status: String,
+    @SerializedName("drink") val drink: String,
+    @SerializedName("statusCondition") val statusCondition: String,
+    @SerializedName("memo") val memo: String,
+    @SerializedName("date") val date: String
+)
+//상태 조회 응답
+data class StatusResponse(
+    @SerializedName("status") val status: String,
+    @SerializedName("drink") val drink: String,
+    @SerializedName("statusCondition") val statusCondition: String,
+    @SerializedName("memo") val memo: String,
+    @SerializedName("date") val date: String
+)
+
+//**************************************************************************//
+
+interface CalendarApi {
+    @GET("/calender/date/{date}")
+    fun getDateDetails(
+        @Path("date") date: String
+    ): Call<CalendarResponse>
+}
+
+//날짜 선택 응답
+data class CalendarResponse(
+    @SerializedName("status") val status: String,
+    @SerializedName("drink") val drink: String,
+    @SerializedName("statusCondition") val statusCondition: String,
+    @SerializedName("memo") val memo: String,
+    @SerializedName("date") val date: String
+)
+
+//**************************************************************************//
+
+// 온보딩 약 검색
+
+interface MedicineApi {
+    @GET("/api/medi/itemName")
+    fun searchMedicines(@Query("itemName") itemName: String): Call<MedicineResponse>
+}
+
+data class MedicineResponse(
+    val medicines: List<Medicine>
+)
+
+data class Medicine(
+    val itemName: String,
+    val itemImage: String?
+)
