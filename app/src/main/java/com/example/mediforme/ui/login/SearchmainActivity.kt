668186229adcb49defbd4b75ea03235dd5ac29ed
seungjae708 <choesj365@gmail.com>
2025-08-25 19:55:00 +0900

    :wrench: [refactor] (#88) Login 부분에 Hilt/코루틴 아키텍처 적용

--- app/src/main/java/com/example/mediforme/ui/login/SearchmainActivity.kt
+++ app/src/main/java/com/example/mediforme/ui/login/SearchmainActivity.kt
@@ -3,8 +3,6 @@ package com.example.mediforme.ui.login
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
--- app/src/main/java/com/example/mediforme/ui/login/ViewPagerAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.mediforme.ui.login
-
-class ViewPagerAdapter {
-}
